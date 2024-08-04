0675eb9d73726b401454b8aa0aedf41d25095b9d
neev0511 <allofbakwas@gmail.com>
2024-08-04 12:35:56 +0530

    Updated Projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,10 +4,6 @@ import mixitup from "mixitup";
 import Popup from "./Popup";
 import VoiceAssistant from "../../assets/img/voice_assistant.webp";
 import DiscordClone from "../../assets/img/discord.png";
-import Newsletter from "../../assets/img/newsletter.png";
-import ToDoList from "../../assets/img/to_do_list.png";
-import FaceFinder from "../../assets/img/face_finder.png";
-import Calendar from "../../assets/img/calendar.png";
 class Work extends Component {
   state = {
     filter: "all",
@@ -39,46 +35,46 @@ class Work extends Component {
         ],
         url: "https://discord-production-3e9b.up.railway.app/",
       },
-      {
-        id: 3,
-        type: "app",
-        title: "Newsletter",
-        image: Newsletter,
-        description:
-          "Developed an app to email daily news using various news APIs, becoming a one-stop shop for daily updates. Automated the process to run daily and send a customizable email digest to users. Implemented user preferences for topic selection and delivery time.",
-        skills: ["Python", "MySQL"],
-        url: "https://github.com/neevj2006/Newsletter/",
-      },
-      {
-        id: 4,
-        type: "web",
-        title: "To-Do List",
-        image: ToDoList,
-        description:
-          "Built a simple and intuitive web application that allows users to create, manage, and track their daily tasks and to-do items, enhancing productivity and organization.",
-        skills: ["React", "Node", "Express", "MongoDB"],
-        url: "https://neevj2006.github.io/To-Do-frontend/",
-      },
-      {
-        id: 5,
-        type: "app",
-        title: "Face Finder",
-        image: FaceFinder,
-        description:
-          "Unlocked the power of facial recognition with a Python application that scans folders to identify photos featuring the user's face, leveraging advanced algorithms to precisely match unique facial features.",
-        skills: ["Python"],
-        url: "https://github.com/neevj2006/Face-Finder",
-      },
-      {
-        id: 6,
-        type: "web",
-        title: "Calendar",
-        image: Calendar,
-        description:
-          "Developed a user-friendly calendar application that streamlines scheduling and task management for school and coding courses, helping users stay organized and manage their time efficiently.",
-        skills: ["React", "Node", "Express", "MongoDB"],
-        url: "https://neevj2006.github.io/Calendar-frontend/",
-      },
+      // {
+      //   id: 3,
+      //   type: "app",
+      //   title: "Newsletter",
+      //   image: Newsletter,
+      //   description:
+      //     "Developed an app to email daily news using various news APIs, becoming a one-stop shop for daily updates. Automated the process to run daily and send a customizable email digest to users. Implemented user preferences for topic selection and delivery time.",
+      //   skills: ["Python", "MySQL"],
+      //   url: "https://github.com/neevj2006/Newsletter/",
+      // },
+      // {
+      //   id: 4,
+      //   type: "web",
+      //   title: "To-Do List",
+      //   image: ToDoList,
+      //   description:
+      //     "Built a simple and intuitive web application that allows users to create, manage, and track their daily tasks and to-do items, enhancing productivity and organization.",
+      //   skills: ["React", "Node", "Express", "MongoDB"],
+      //   url: "https://neevj2006.github.io/To-Do-frontend/",
+      // },
+      // {
+      //   id: 5,
+      //   type: "app",
+      //   title: "Face Finder",
+      //   image: FaceFinder,
+      //   description:
+      //     "Unlocked the power of facial recognition with a Python application that scans folders to identify photos featuring the user's face, leveraging advanced algorithms to precisely match unique facial features.",
+      //   skills: ["Python"],
+      //   url: "https://github.com/neevj2006/Face-Finder",
+      // },
+      // {
+      //   id: 6,
+      //   type: "web",
+      //   title: "Calendar",
+      //   image: Calendar,
+      //   description:
+      //     "Developed a user-friendly calendar application that streamlines scheduling and task management for school and coding courses, helping users stay organized and manage their time efficiently.",
+      //   skills: ["React", "Node", "Express", "MongoDB"],
+      //   url: "https://neevj2006.github.io/Calendar-frontend/",
+      // },
     ],
     popup: false,
     popupContent: {},
